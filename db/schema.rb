9f05e6d73ee9decded80f82366d415cfd4e7a8c7
竹内瑳彩 <tahiti.hawaii.29@gmail.com>
2023-06-13 11:07:56 +0900

    content カラムを排除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_13_014716) do
+ActiveRecord::Schema.define(version: 2023_06_13_015558) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,7 +49,6 @@ ActiveRecord::Schema.define(version: 2023_06_13_014716) do
   create_table "articles", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.string "title", null: false
-    t.text "content", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_articles_on_user_id"
