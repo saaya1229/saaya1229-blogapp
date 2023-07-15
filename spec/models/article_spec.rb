6d2da58d7c7d07632d70f117e2007b4aaaea9f96
竹内瑳彩 <tahiti.hawaii.29@gmail.com>
2023-07-15 15:45:46 +0900

    article factoryを登録

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -4,12 +4,7 @@ RSpec.describe Article, type: :model do
   let!(:user) { create(:user) }
 
   context 'タイトルと内容が入力されている場合' do
-    let(:article) do
-      user.articles.build({
-        title: Faker::Lorem.characters(number: 10),
-        content: Faker::Lorem.characters(number: 300)
-      })
-    end
+    let(:article) { build(:article, user: user) }
 
     it '記事を保存できる' do
       expect(article).to be_valid
@@ -17,14 +12,15 @@ RSpec.describe Article, type: :model do
   end
 
   context 'タイトルの文字が１文字の場合' do
-    let(:article) do
-      user.articles.create({
-        title: Faker::Lorem.characters(number: 1),
-        content: Faker::Lorem.characters(number: 300)
-      })
+    let(:article) { build(:article, title: Faker::Lorem.characters(number: 1), user: user) }
+
+
+    before do
+      article.save
     end
 
     it '記事を保存できない' do
+      article.save
       expect(article.errors.messages[:title][0]).to eq('は2文字以上で入力してください')
     end
   end
